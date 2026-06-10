271247566ad91cf2e14a3e6e2b1c473b3c1faa82
Hüseyin Akaslan <hsynaksln@gmail.com>
2026-06-10 12:46:33 +0300

    add port for mobile access

--- mobile/constants/config.ts
+++ mobile/constants/config.ts
@@ -1,5 +1,5 @@
 // Gerçek telefonla test: bilgisayarının Wi-Fi IP'sini yaz (ipconfig → IPv4 Address)
 // Android emulator için: 10.0.2.2
-export const API_BASE_URL = "http://192.168.1.34:8080";
-export const STORAGE_BASE_URL = "http://192.168.1.34:9000";
+export const API_BASE_URL = "http://10.184.148.40:8081";
+export const STORAGE_BASE_URL = "http://10.184.148.40:9000";
 export const IMAGE_BUCKET = "headphoneapi-product-images";
